b2069ac55a9c372ff31e6e987c4af96d89fdbfbb
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-08-28 12:05:22 -0300

    Syscall e sll

---
+++ syscall-sll.asm
@@ -0,0 +1,17 @@
+.text
+main: addi $2, $0, 5
+      syscall
+      
+      addi $8, $2, 0
+      addi $4, $2, 0
+      addi $2, $0, 1
+      syscall
+      
+      addi $4, $0, ' '
+      addi $2, $0, 11
+      syscall
+      
+      sll $4, $8, 1
+      addi $2, $0, 1
+      syscall
+      
