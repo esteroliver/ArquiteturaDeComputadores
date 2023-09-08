31ced70b52024893cf8abc5f3fa952aea394bf46
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-09-08 10:32:06 -0300

    beq

---
+++ 3-estrutura-decisao/beq.asm
@@ -0,0 +1,26 @@
+.text
+main:
+	addi $2, $0, 5
+	syscall
+	add $8, $0, $2
+	addi $2, $0, 5
+	syscall
+	add $9, $0, $2
+	
+	beq $8, $9, eq
+	
+	# se $8 != $9
+	addi $4, $0, 'x'
+	
+	j imp
+	
+eq:	
+	# se $8 == $9
+	addi $4, $0, '='
+	
+imp:
+	addi $2, $0, 11
+	syscall
+	
+	addi $2, $0, 10
+	syscall 
