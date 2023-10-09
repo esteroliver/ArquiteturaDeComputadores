a7f790fa4d99ff71c81fe6f8edfb165c1996c034
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-10-09 14:50:46 -0300

    Update q2-pr.asm

--- lista03/q2-pr.asm
+++ lista03/q2-pr.asm
@@ -3,27 +3,46 @@ main:
 	addi $8, $0, 999
 	addi $9, $0, 999
 	
-laco:
-	mul $10, $8, $9
-	addi $11, $0, 0
-	addi $9, $9, -1
+dados:
+	addi $10, $0, 10 # vamos precisar desse valor com frequência
+	addi $11, $0, 10000 # i
+	addi $12, $0, 1 # n
+	addi $13, $0, 10 # contador
+	addi $14, $0, 0 # num_invertido
+	mul $15, $8, $9 # num
 	
-inverter:
-	slt $14, $10, $0
-	bne $14, $0, final
-	addi $12, $0, 10
-	div $10, $12
-	mfhi $13
-	mul $11, $11, $12
-	add $11, $11, $13
-	mflo $10
-	j inverter
-final:
-	beq $11, $10, fim
-	addi $15, $15, 1
+	# ALGORITMO PARA PEGAR NÚMERO INVERTIDO
+	# while (i > 0){
+		# num_invertido += ((num%contador) * i) / n;
+		# num -= num%contador;
+		# contador *= 10;
+		# n *= 10;
+		# i = i/10;
+	# }
+laco:
+	beq $11, $0, fim_laco
+	
+	div $15, $13
+	mfhi $17
+	sub $15, $15, $17
+	mul $17, $17, $11
+	div $17, $12
+	mflo $17
+	add $14, $14, $17
+	
+	mul $13, $13, $10
+	mul $12, $12, $10
+	div $11, $10
+	mflo $11
+	
 	j laco
-fim:
-	add $4, $11, $0
+fim_laco:
+	mul $15, $8, $9
+	beq $14, $15, palindromos
+	addi $8, $8, -1
+	j dados
+palindromos:
+	add $4, $14, $0
 	addi $2, $0, 1
 	syscall
 	
